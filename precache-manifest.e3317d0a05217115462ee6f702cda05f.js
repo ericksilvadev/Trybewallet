bc361e61fa6230fe79ff1a2e146d4f593a633191
Erick Silva <erick.silva6653@gmail.com>
2021-08-31 14:33:53 -0300

    Updates

---
+++ precache-manifest.e3317d0a05217115462ee6f702cda05f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54db4632304832e20cba4bd5f59a107c",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "bf41147f3bdfee9f541b",
+    "url": "/trybewallet/static/css/main.45ba1e34.chunk.css"
+  },
+  {
+    "revision": "c7d53e8c4a87a44e8f4e",
+    "url": "/trybewallet/static/js/2.86786e65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.86786e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf41147f3bdfee9f541b",
+    "url": "/trybewallet/static/js/main.55b427dd.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
